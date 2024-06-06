ad3d21f89cdcf2ba518e1678dab17ed0e06b549f
Sabah Yusuf <sabah.yusuf@outlook.com>
2024-06-06 12:36:58 +0100

    add line 7

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ public class App {
         System.out.println("Hello World!");
         System.out.println("Added this on line 4");
         System.out.println("added this line to line 5 in feature b");
+        System.out.println("added line to make a feature c!");
     }
 
 }
